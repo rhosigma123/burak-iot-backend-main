fe0de1a85dc8338152f818c8b6f71c11a01388a1
MisTech Solutions <mistech.co@gmail.com>
2024-05-15 22:57:09 +0530

    all cors allowed

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const createApp = (): Express => {
     const server = http.createServer(app);
     const io = new Server(server, {
         cors: {
-            origin: CLIENT_URL,
+            origin: "*",
         },
         transports: ["websocket", "polling"],
     });
